e4d5cea86fc4893861a60c077a4670c756b2f360
crbrk <cbrk@protonmail.com>
2019-02-01 17:27:00 +0100

    path change

--- WordCloud/urls.py
+++ WordCloud/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wordcloudgenerator/', include('WordCloudGenerator.urls')),
+    path('', include('WordCloudGenerator.urls')),
 ]
